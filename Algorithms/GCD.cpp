d76b320f0a6543a612a70a2b88371f09bb1633d2
Hristo Todorov <httodoroff@gmail.com>
2019-08-03 16:36:18 +0300

    Create GCD.cpp

---
+++ Algorithms/GCD.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    if(b==0) {
+        return a;
+    }
+    gcd(b, a%b);
+}
+
+int hcf(int a, int b) {
+    return (a/gcd(a, b))*b;
+}
+
+int main()
+{
+    int a, b;
+    cout<<gcd(a, b)<<" "<<hcf(a, b);
+    
+    return 0;
+}
